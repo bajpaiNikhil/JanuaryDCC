84c96d6ffe607755afd9269c38f270438b9b8c0f
NikhilBajpai <nikhil.cse16@gmail.com>
2022-01-15 18:26:43 +0530

    day9

--- 566. Reshape the Matrix.py
+++ 566. Reshape the Matrix.py
@@ -2,3 +2,22 @@ mat = [[1, 2], [3, 4]]
 r = 1
 c = 4
 
+rowLength = len(mat)
+colmLength = len(mat[0])
+
+if rowLength*colmLength != r*c:
+    print(mat)
+
+l = []
+for i in range(rowLength):
+    for j in range(colmLength):
+        l.append(mat[i][j])
+print(l)
+
+k= 0
+new_mat = list()
+for i in range(r):
+    print(i)
+    new_mat.append(l[k:k+c])
+    k+=c
+print(new_mat)
