1158649068525b738b45292b26ab97271a43884b
NikhilBajpai <nikhil.cse16@gmail.com>
2022-01-23 16:47:45 +0530

    day16

--- 119. Pascal's Triangle II.py
+++ 119. Pascal's Triangle II.py
@@ -1,2 +1,19 @@
-rowIndex = 5
+rowIndex = 3
 
+triangle = [[1] , [1,1]]
+
+
+if rowIndex == 0:
+    print(triangle[0])
+elif rowIndex == 2:
+    print(triangle[1])
+
+else:
+    res = []
+    for i in range(2 , rowIndex+1):
+        for j in range(i-1):
+            res.append(sum(triangle[-1][j:j+2]))
+        triangle.append([1] +res+[1])
+        res = []
+    print(len(triangle))
+    print(triangle[-1] , )
