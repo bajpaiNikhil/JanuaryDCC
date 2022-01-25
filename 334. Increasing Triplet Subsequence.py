a44ac28d3e021b86aae75e1d94287c2761f72d77
NikhilBajpai <nikhil.cse16@gmail.com>
2022-01-25 20:57:30 +0530

    day18

---
+++ 334. Increasing Triplet Subsequence.py
@@ -0,0 +1,15 @@
+# nums = [1,2,3,4]
+# p = 1
+# output =  []
+# for i in nums:
+#     output.append(p)
+#     p=p*i
+# p = 1
+# print(output)
+# for i in range(len(nums)-1,-1,-1):
+#     print(i)
+#     output[i] = output[i]*p
+#     p = p*nums[i]
+# print(output)
+nums = [5,4,3,2,1]
+
